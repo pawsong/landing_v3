24144a92796bdd76f9552e8f6a62a6c44d3b5b71
joundr <limchrm@gmail.com>
2015-09-25 16:13:51 +0900

    added files

---
+++ js/chrmdemo.js
@@ -0,0 +1,25 @@
+(function(){                                                                     
+
+	angular.module('chrmList', [
+	]).controller('chrmController', [function() {
+		this.slides = data;
+	}]);
+
+	var data = [
+
+	{
+		name: '김진우',
+		date: '2015. 9. 19. AM 12:26',
+		market: 'google',
+		says: '너무 좋아요! 이거 커플이랑 하기 너무 좋은 거 같아요. 특히 커플끼리 추억을 기록한다는 게 좋아요 :)'
+	},
+	{
+		name: '김진우',
+		date: '2015. 9. 19. AM 12:26',
+		market: 'google',
+		says: '너무 좋아요! 이거 커플이랑 하기 너무 좋은 거 같아요. 특히 커플끼리 추억을 기록한다는 게 좋아요 :)'
+	}
+
+	];
+
+})();  
